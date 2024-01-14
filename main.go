761c7f9dd36fef70eef4c7696bbb947966c393f0
Chinonso <chinonsoalexander2@gmail.com>
2024-01-14 19:49:08 +0100

    cron test with render

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/robfig/cron/v3"
 )
 
+var (
+	once         sync.Once
+	wg           sync.WaitGroup
+	firstRequest bool
+)
+
 func myScheduledTask() {
+	defer wg.Done() // Decrement the wait group counter when the task is done
 	fmt.Println("Executing scheduled task: ", time.Now())
 	makeHTTPRequest()
 }
@@ -37,6 +45,31 @@ func startWebServer() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		once.Do(func() {
+			// Increment the wait group counter on the first request
+			wg.Add(1)
+
+			// Schedule the task to run every minute after the first request
+			go func() {
+				// Wait for the main function to finish before starting the cron job
+				wg.Wait()
+
+				// Create a new cron scheduler
+				cronScheduler := cron.New()
+
+				// Schedule the task to run every minute
+				_, err := cronScheduler.AddFunc("* * * * *", myScheduledTask)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// Start the cron scheduler
+				cronScheduler.Start()
+			}()
+		})
+
+		firstRequest = true
+
 		return c.SendString("Hello, this is your web service!")
 	})
 
@@ -44,21 +77,6 @@ func startWebServer() {
 }
 
 func main() {
-	// Create a new cron scheduler
-	c := cron.New()
-
-	// Schedule the task to run every minute
-	_, err := c.AddFunc("* * * * *", myScheduledTask)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Start the cron scheduler in a goroutine
-	go c.Start()
-
 	// Run the web server
 	startWebServer()
-
-	// Keep the main goroutine running
-	select {}
 }
